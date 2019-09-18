567caddb26b4f4b423bdd1b9ecc28ba191b80894
ningh <996099092@qq.com>
2019-09-18 14:17:51 +0800

    jdk13新特性

---
+++ src/com/nice/Main.java
@@ -0,0 +1,32 @@
+package com.nice;
+
+import static javax.swing.text.html.HTML.Tag.SELECT;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        int i = 0;
+//      int c =   switch (i) {
+//            case 1 -> 3;
+//            default -> 5;
+//        };
+//        System.out.println(c);
+//        test1();
+        
+
+
+    }
+
+//    public static  void test1(){
+//        int c = 2;
+//        int i = switch (c) {
+//            case 0 : yield  1;
+//            default :{
+//                yield  2;
+//            }
+//        };
+//        System.out.println(c);
+//    }
+
+}
